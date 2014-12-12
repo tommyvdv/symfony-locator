320f4af0af5f59497fa672df17350c1e3d896188
Tommy Van de Velde <tommy@wijs.be>
2014-12-12 09:44:42 +0100

    ## doing
    - set up paging
    - set up locator
    - set up entities
    - set up repository
    - add map
    
    ## todo
    
    - configure map
    
    ## done
    
    - setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Bazinga\Bundle\GeocoderBundle\BazingaGeocoderBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Locator/LocationBundle/Controller/PageController.php
+++ src/Locator/LocationBundle/Controller/PageController.php
@@ -8,10 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Locator\LocationBundle\Entity\Address;
 use Locator\LocationBundle\Form\AddressType;
 
+use Ivory\GoogleMap\Overlays\Animation;
+use Ivory\GoogleMap\Overlays\Marker;
+
 class PageController extends Controller
 {
     public function indexAction()
     {
+        // map
+        $map = $this->get('ivory_google_map.map');
+
         // form
         $latlng = null;
         $address = new Address();
@@ -23,6 +29,10 @@ class PageController extends Controller
             if ($form->isValid()) {
                 // Perform some action, such as sending an email
                 $latlng = $address->getLatLng();
+
+                // center on lookup
+                $map->setCenter($latlng['lat'], $latlng['lng'], true);
+                $map->setMapOption('zoom', 10);
             }
         }
 
@@ -33,10 +43,21 @@ class PageController extends Controller
         $locations = $em->getRepository('LocatorLocationBundle:Location')
                     ->getAll(null, $latlng);
 
+        foreach ($locations as $location)
+        {
+            $marker = new Marker();
+            $marker->setPosition($location->getLatitude(), $location->getLongitude(), true);
+            $marker->setAnimation(Animation::DROP);
+            $marker->setOption('flat', true);
+
+            $map->addMarker($marker);
+        }
+
         // render
         return $this->render('LocatorLocationBundle:Page:index.html.twig', array(
             'locations' => $locations,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'map' => $map
         ));
     }
     public function aboutAction()
